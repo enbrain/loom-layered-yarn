89f43700b889ae94dee71745b7011d9d119d99ec
enbrain <69905075+enbrain@users.noreply.github.com>
2022-02-05 09:34:51 +0900

    Update fabric-loom to 0.11.29

--- src/main/java/io/github/enbrain/loomlayeredyarn/yarn/YarnMappingsLayer.java
+++ src/main/java/io/github/enbrain/loomlayeredyarn/yarn/YarnMappingsLayer.java
@@ -3,29 +3,28 @@ package io.github.enbrain.loomlayeredyarn.yarn;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Supplier;
 
 import io.github.enbrain.loomlayeredyarn.util.MappingHelper;
 import net.fabricmc.loom.api.mappings.layered.MappingLayer;
 import net.fabricmc.loom.api.mappings.layered.MappingsNamespace;
+import net.fabricmc.loom.configuration.providers.mappings.file.FileMappingsLayer;
 import net.fabricmc.loom.configuration.providers.mappings.intermediary.IntermediaryMappingLayer;
-import net.fabricmc.mappingio.MappingUtil;
 import net.fabricmc.mappingio.MappingVisitor;
-import net.fabricmc.mappingio.adapter.MappingNsRenamer;
-import net.fabricmc.mappingio.adapter.MappingSourceNsSwitch;
 import net.fabricmc.mappingio.tree.MemoryMappingTree;
 
-public record YarnMappingsLayer(Path source, Supplier<MemoryMappingTree> intermediarySupplier) implements MappingLayer {
-
-    public static Map<String, String> ENIGMA_NAMESPACE_MAP = Map.of(MappingUtil.NS_SOURCE_FALLBACK, MappingsNamespace.INTERMEDIARY.toString(), MappingUtil.NS_TARGET_FALLBACK, MappingsNamespace.NAMED.toString());
+public record YarnMappingsLayer(Path source, Supplier<MemoryMappingTree> intermediary) implements MappingLayer {
 
     @Override
-    public void visit(MappingVisitor dest) throws IOException {
-        MappingVisitor validator = new MappingValidator(dest, intermediarySupplier.get());
-        MappingVisitor nsSwitch = new MappingSourceNsSwitch(validator, getSourceNamespace().toString());
-        MappingVisitor nsRenamer = new MappingNsRenamer(nsSwitch, ENIGMA_NAMESPACE_MAP);
-        MappingHelper.read(source, nsRenamer);
+    public void visit(MappingVisitor mappingVisitor) throws IOException {
+        MappingHelper.findMappingPath(source, p -> {
+            try {
+                FileMappingsLayer fileMappingsLayer = new FileMappingsLayer(p, "", MappingsNamespace.INTERMEDIARY.toString(), MappingsNamespace.NAMED.toString(), false, this.getSourceNamespace().toString());
+                fileMappingsLayer.visit(new MappingValidator(mappingVisitor, this.intermediary.get()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @Override
